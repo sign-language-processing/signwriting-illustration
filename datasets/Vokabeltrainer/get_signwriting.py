6a63aed1c3ecb20ae75ee326b12d73860803771b
Amit Moryossef <amitmoryossef@gmail.com>
2023-11-24 17:07:34 +0100

    fix(Vokabeltrainer): add more gloss mappings

--- datasets/Vokabeltrainer/get_signwriting.py
+++ datasets/Vokabeltrainer/get_signwriting.py
@@ -5,6 +5,49 @@ import re
 import csv
 
 
+def ascii_gloss(gloss: str):
+    return gloss.replace('ä', '').replace('ö', '').replace('ü', '')
+
+
+def parse_lexicon_tsv(name: str):
+    res = {}
+    with open(name + '.tsv', 'r', encoding='utf-8') as file:
+        dict_reader = csv.DictReader(file, delimiter='\t')
+
+        for row in dict_reader:
+            gloss = row['Glosse']
+            nr = row['Nr']
+            # Happens that some of the entries are swapped
+            try:
+                int(nr)
+            except:
+                gloss, nr = nr, gloss
+
+            gloss = gloss.lower()
+            res[gloss] = int(nr)
+            res[ascii_gloss(gloss)] = int(nr)
+
+    return res
+
+def parse_gloss_map():
+    res = {}
+    with open('Vokabeltrainer-Glossen.txt', 'r') as f:
+        for line in f.readlines():
+            gloss, nr = line.split('\t')
+            gloss = gloss.lower()
+            res[gloss] = int(nr)
+            res[ascii_gloss(gloss)] = int(nr)
+    return res
+
+
+lexicon = parse_lexicon_tsv('lexicon')
+wortschatz = parse_lexicon_tsv('wortschatz')
+gloss_map = parse_gloss_map()
+
+lexicon.update(wortschatz)
+lexicon.update(gloss_map)
+
+
 def convert_to_fsw(layout_txt_content):
     # Fixing img tags to be self-closing for XML parsing
     layout_txt_content = re.sub(r'<img([^>]+)/?>', r'<img\1/>', layout_txt_content)
@@ -21,7 +64,30 @@ def convert_to_fsw(layout_txt_content):
     return fsw
 
 
-def process_directory(directory, lexicon):
+def get_gloss_ids(gloss: str):
+    # Option 1: Id is a 3-5 digit number within the gloss, Return all matches
+    matches = re.findall(r'\d{3,5}', gloss)
+    if len(matches) > 0:
+        for match in matches:
+            yield int(match)
+
+    # Option 2: gloss is in the lexicon, return the id
+    lower_goss = gloss.lower()
+    if lower_goss in lexicon:
+        yield lexicon[lower_goss]
+    elif ascii_gloss(lower_goss) in lexicon:
+        yield lexicon[ascii_gloss(lower_goss)]
+
+    # Try adding "A" to the end
+    new_gloss = lower_goss.replace('-', '_') + 'a'
+    if new_gloss in lexicon:
+        yield lexicon[new_gloss]
+    elif ascii_gloss(new_gloss) in lexicon:
+        yield lexicon[ascii_gloss(new_gloss)]
+
+
+
+def process_directory(directory):
     results = []
     for dir_name, _, _ in os.walk(directory):
         layout_path = os.path.join(dir_name, 'layout.txt')
@@ -30,28 +96,25 @@ def process_directory(directory, lexicon):
                 content = file.read()
                 fsw = convert_to_fsw(content)
                 name = os.path.basename(dir_name)
-                if name not in lexicon:
+                gloss_ids = list(get_gloss_ids(name))
+                if len(gloss_ids) == 0:
                     print(f"Missing {name}")
                     continue
-                results.append({
-                    # need to pad Nr to 5 digits
-                    "file": f"illustrations/{lexicon[name]['Nr'].zfill(5)}.png",
-                    "fsw": fsw,
-                    "meta": lexicon[name]
-                })
 
+                for _id in gloss_ids:
+                    file_path = f"illustrations/{str(_id).zfill(5)}.png"
+                    if os.path.isfile(file_path):
+                        results.append({
+                            "file": file_path,
+                            "fsw": fsw,
+                            # "meta": lexicon[name]
+                        })
+
+    print('Found', len(results))
     with open('writing.json', 'w') as json_file:
-        json.dump(results, json_file)
-
-
-def parse_lexicon_tsv():
-    with open('lexicon.tsv', 'r', encoding='utf-8') as file:
-        dict_reader = csv.DictReader(file, delimiter='\t')
-        # Convert the reader to a list to return it
-        return {row['Glosse']: row for row in dict_reader}
+        json.dump(results, json_file, indent=2)
 
 
 if __name__ == "__main__":
-    lexicon = parse_lexicon_tsv()
-    print(len(list(lexicon.keys())))
-    process_directory('SW_signs_glosses', lexicon)
+    # lexicon = parse_lexicon_tsv()
+    process_directory('SW_signs_glosses')
